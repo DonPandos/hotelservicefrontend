0a67d41112deef6799b96004ae1ed7b847bd49ec
DonPandos <ewrr_96@mail.ru>
2020-12-18 08:50:32 +0300

    Some changes

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,21 +1,35 @@
 import react, { useEffect, useState } from 'react'
 import { useForm } from "react-hook-form";
+import API from '../../utils/API'
 
 export default function LoginForm() {
 
     const {register, handleSubmit} = useForm();
 
-    const handleOnSubmit = (data) => alert(JSON.stringify(data));
+    const handleOnSubmit = (data) => {
+        alert(JSON.stringify(data));
+        const response =  API.post("/auth/login", data, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response);
+    }
 
     useEffect(() => {
-        console.log(count);
     });
 
     return (
         <div>
             <form onSubmit={handleSubmit(handleOnSubmit)}>
-                <input name="username" ref={register}></input>
-                <button type="submit">asd</button>
+                <p>
+                    <input name="username" ref={register} placeholder="Логин"></input>
+                </p>
+                <p>
+                    <input type="password" name="password" ref={register} placeholder="Пароль"></input>
+                </p>
+                <button type="submit">Войти</button>
             </form>
         </div>
     );
