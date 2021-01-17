6f08906d4e1dc127071d7cf5fdf7aaec92d54523
Bogdan <ewrr_96@mail.ru>
2021-01-17 20:56:26 +0300

    Nice commit

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -2,12 +2,13 @@ import s from './FilterBar.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {setHotels} from "../../reducers/hotel/actions";
 import API from "../../utils/API";
+import {useEffect, useState} from "react";
 
 const FilterBar = (props) => {
     const cities = useSelector(state => state.city.cities);
-    console.log(cities)
     const citiesMap = cities.map(cityItem => <option value={cityItem.id}>{cityItem.name}</option>)
     const dispatch = useDispatch();
+    const [ facilities, setFacilities ] = useState([]);
 
     const handleOnCityChanged = (event) => {
         if(event.target.value == 'all') {
@@ -22,6 +23,37 @@ const FilterBar = (props) => {
                 dispatch(setHotels(response.data));
             })
     }
+
+    const handleOnFacilityChanged = (event) => {
+        if(event.target.checked) {
+            setFacilities(facilities.concat([event.target.value]));
+        } else {
+            setFacilities(facilities.filter((item) => {
+                return item !== event.target.value;
+            }))
+        }
+    }
+
+    useEffect(() => {
+        if(facilities.length == 0) {
+            API.get('/hotels')
+                .then((response) => {
+                    dispatch(setHotels(response.data))
+                });
+        } else {
+            API.get('/hotels/facilities?facilitiesId=' + facilities.map(el => el), {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then((response) => {
+                    console.log(response.data)
+                    dispatch(setHotels(response.data))
+                })
+        }
+    }, [facilities])
+
     return (
         <div className={s.root}>
             <div className={s.label}><p>Город</p></div>
@@ -32,11 +64,11 @@ const FilterBar = (props) => {
             <div className={s.label}><p>Удобства</p></div>
             <div className={s.facilities}>
                 <ul>
-                    <li><input type="checkbox" name={'wifi'}/><label for={'wifi'}>Wi-Fi</label></li>
-                    <li><input type="checkbox" name={'wifi'}/><label for={'wifi'}>Парковка</label></li>
-                    <li><input type="checkbox" name={'wifi'}/><label for={'wifi'}>Бар</label></li>
-                    <li><input type="checkbox" name={'wifi'}/><label for={'wifi'}>Теннис</label></li>
-                    <li><input type="checkbox" name={'wifi'}/><label for={'wifi'}>СПА</label></li>
+                    <li><input type="checkbox" value={'1'} name={'wifi'} onChange={handleOnFacilityChanged}/><label for={'wifi'}>Wi-Fi</label></li>
+                    <li><input type="checkbox" value={'2'} name={'parking'} onChange={handleOnFacilityChanged}/><label for={'wifi'}>Парковка</label></li>
+                    <li><input type="checkbox" value={'3'} name={'bar'} onChange={handleOnFacilityChanged}/><label for={'wifi'}>Бар</label></li>
+                    <li><input type="checkbox" value={'4'} name={'tennis'} onChange={handleOnFacilityChanged}/><label for={'wifi'}>Теннис</label></li>
+                    <li><input type="checkbox" value={'5'} name={'spa'} onChange={handleOnFacilityChanged}/><label for={'wifi'}>СПА</label></li>
                 </ul>
             </div>
         </div>
--- src/components/HotelInfo/HotelInfo.jsx
+++ src/components/HotelInfo/HotelInfo.jsx
@@ -1,10 +1,12 @@
 import s from './HotelInfo.module.css'
 import {NavLink} from "react-router-dom";
+import HotelItem from "../HotelList/HotelItem";
 
 const HotelInfo = (props) => {
 
     const src = 'data:image/png;base64,' + props.location.image;
-    console.log(props.location)
+
+    const facilitiesMap = props.location.facilities.map(facility => <p>{facility.name}</p>)
     return (
         <div className={s.root}>
             <div className={s.image}>
@@ -16,12 +18,13 @@ const HotelInfo = (props) => {
                     <div className={s.city}>{props.location.city.name}</div>
                 </div>
                 <div className={s.ratingBlock}>
-                    {/*<div className={s.ratingItem}>{props.location.rating}</div>*/}
+                    <div className={s.ratingItem}>{props.location.rating}</div>
                 </div>
                 <div className={s.description}>{props.location.description}</div>
                 <div className={s.moreBlock}>
 
                 </div>
+                {facilitiesMap}
             </div>
         </div>
     )
